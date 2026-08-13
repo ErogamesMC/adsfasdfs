0e11963dc32786f1feee2ae09463b5daacf21617
NovariaMC <erogames1.123@gmail.com>
2026-08-13 00:34:32 -0700

    feat: add MainActivity

---
+++ app/src/main/java/com/app/msf:1000275838/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.app.msf:1000275838
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
