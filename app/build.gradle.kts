1325d75dfc55329f6318f38f076f9c80bd2cd5be
NovariaMC <erogames1.123@gmail.com>
2026-08-13 00:34:29 -0700

    chore: update app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.app.msf:1000275838"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.app.msf:1000275838"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
